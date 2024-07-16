d1ba3ef7eb231ff46e06b09685afce0446d17de9
Shanol11 <109813804+Shanol11@users.noreply.github.com>
2024-07-16 22:37:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ $stmt->bind_param("sss", $name, $email, $password);
 
 if ($stmt->execute()) {
     
-    echo "<script>window.location.href='https://shanol11.github.io/restaurant-managementsystem/login.php";
+    echo "<script>window.location.href='https://shanol11.github.io/restaurant-managementsystem/home.html";
 } else {
     echo "Error: " . $stmt->error;
 }
